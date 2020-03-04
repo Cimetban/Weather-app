2befbc0a243c3b3f94d5f2c43d89c24805ae6445
unknown <bile.geo@hotmail.com>
2020-03-04 20:55:24 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+
+
+
+const inputForm=document.querySelector('form');
+const search=document.querySelector('input');
+
+const messageOne=document.querySelector('h3')
+const messageTwo=document.querySelector('#forecastText')
+
+
+inputForm.addEventListener('submit', (e)=>{
+ event.preventDefault()
+
+ const location=search.value
+
+
+ messageOne.textContent='Loading'
+ messageTwo.textContent=''
+
+
+
+  fetch('http://localhost:3000/weather?address=' +location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+          messageOne.textContent=data.error
+        } else {
+        messageOne.textContent= data.location
+        messageTwo.textContent= data.forecast
+        }
+
+    })
+
+    })
+})
+
